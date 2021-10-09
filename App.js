dd1e79ff9084200d86c9a453a13fe89d6b75b677
Matt Setaro <fouulsy@gmail.com>
2021-10-08 20:12:56 -0400

    working with navigation

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import MainScreen from './screens/MainScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="is this thing on?" onPress={() => alert("yo")} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={MainScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/MainScreen.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function MainScreen() {
+
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
